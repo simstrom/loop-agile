d525e5d910b34f8fbca67dc2963da6e523e07a3a
Simon Nystrom <simons.nystrom@gmail.com>
2023-01-24 17:21:50 +0100

    updated env variables to work with react

--- client/src/utils/firebase.config.js
+++ client/src/utils/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: process.env.API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	projectId: process.env.PROJECT_ID,
-	storageBucket: process.env.BUCKET,
-	messagingSenderId: process.env.SENDER_ID,
-	appId: process.env.APP_ID,
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_BUCKET,
+	messagingSenderId: process.env.REACT_APP_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
