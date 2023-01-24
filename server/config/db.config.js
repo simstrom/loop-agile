0a84f54379640987e7c1bd4d45eaaadbdad31859
Simon Nystrom <simons.nystrom@gmail.com>
2023-01-24 12:05:41 +0100

    updated db and deployment config details

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,10 +3,5 @@ require('dotenv').config();
 // DB Connection details. Set in .env file.
 const env = process.env;
 module.exports = {
-	HOST: env.DB_HOST,
-	USER: env.DB_USER,
-	PASSWORD: env.DB_PASSWORD,
-	DATABASE: env.DB_NAME,
 	URI: env.DB_URI,
-	DIALECT: 'postgres',
 };
